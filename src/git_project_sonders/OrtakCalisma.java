453310410dcbdb968d9ed06074fd6d6c85a088e8
kemgur <kemgur52@gmail.com>
2020-08-16 16:07:51 -0400

    feature

--- src/git_project_sonders/OrtakCalisma.java
+++ src/git_project_sonders/OrtakCalisma.java
@@ -3,7 +3,7 @@ package git_project_sonders;
 public class OrtakCalisma {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Kemal'den Selamlar...");
 
 	}
 
